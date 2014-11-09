1bbdef255ed95268f1121291e9f54f73136d536e
John Hawthorn <john.hawthorn@gmail.com>
2014-11-08 18:55:27 -0800

    Add interactive mode to bin/lune

--- lune.lua
+++ lune.lua
@@ -9,13 +9,13 @@ local lune = {
   parse = require('lune.parse');
   compile_ast = require('lune.compile');
   compile = function(string)
-    return lune.compile_ast(lune.parse(string))
+    return lune.compile_ast(assert(lune.parse(string)))
   end;
   loadstring = function(string)
-    return loadstring(lune.compile(string))
+    return loadstring(assert(lune.compile(string)))
   end;
   eval = function(string)
-    lune.loadstring(string)()
+    assert(lune.loadstring(string))()
   end
 }
 
